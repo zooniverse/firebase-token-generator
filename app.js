e5b3f48507459637699310deed87818a3be933a7
Simone Duca <simone@zooniverse.org>
2016-09-20 15:59:19 +0100

    Fix #4

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
-var firebase = require("firebase");
-var api = require("panoptes-client")
-var config = require("./config.json")
-
+var firebase = require('firebase');
+var apiClient = require('panoptes-client/lib/api-client');
+var config = require('./config.json');
 
 // Constants
 const PORT = 8080;
-
 
 // Initialise the Firebase SDK for Node.js
 // config.json should contains the Firebase service account keys, read more https://firebase.google.com/docs/server/setup#initialize_the_sdk
@@ -14,6 +12,34 @@ firebase.initializeApp({
   serviceAccount: config,
   databaseURL: "https://project-6243802502502885389.firebaseio.com",
 });
+
+
+// Check if token is valid string
+function isValidToken(string) {
+  if (string.match(/([\w\-\.]+)/g)) {
+    console.log('Token is valid')
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Check Panoptes session
+function sessionExists(token) {
+  console.log('Getting session');
+  apiClient.headers.Authorization = 'Bearer ' + token;
+  return apiClient.get('/me')
+    .then(function(users) {
+      var user = users[0];
+      console.log('Got session', user.login, user.id);
+      return user;
+    })
+    .catch(function(error) {
+      console.error('Failed to get session');
+      throw error;
+    });
+}
 
 
 // App
@@ -26,8 +52,15 @@ app.use(function(req, res, next) {
 });
 
 app.get('/validate', function (req, res, next) {
-  const token = firebase.auth().createCustomToken(req.query.token);
-  res.json({ token });
+  if (isValidToken(req.query.token)) {
+    sessionExists(req.query.token).then(function() {
+      const token = firebase.auth().createCustomToken(req.query.uid);
+      console.log('Firebase token is', token)
+      res.json({ token });
+    })
+  } else {
+    console.log('Invalid token')
+  }
 });
 
 app.listen(PORT, function () {
