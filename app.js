6d66604ced14b3b2ab0fc0e4ac93857f25c3a264
Roger Hutchings <roger@hutchin.gs>
2016-09-21 22:07:15 +0100

    Misc code fixes
    - Consistently use es6 features
    - try and trim code down where poss

--- app.js
+++ app.js
@@ -1,55 +1,50 @@
-var express = require('express');
-var firebase = require('firebase');
-var config = require('./config.json');
-
-var fetch = require('isomorphic-fetch');
+const express = require('express');
+const firebase = require('firebase');
+const fetch = require('isomorphic-fetch');
+const config = require('./config.json');
 
 // Constants
 const PORT = 8080;
-const productionUrl = 'https://www.zooniverse.org/api';
-const stagingUrl = 'https://panoptes-staging.zooniverse.org/api';
-const API_URL = (process.env.NODE_ENV === 'production') ? productionUrl : stagingUrl;
+const PRODUCTION_URL = 'https://www.zooniverse.org/api';
+const STAGING_URL = 'https://panoptes-staging.zooniverse.org/api';
+const API_URL = (process.env.NODE_ENV === 'production') ? PRODUCTION_URL : STAGING_URL;
 
 // Initialise the Firebase SDK for Node.js
-// config.json should contain the Firebase service account keys, read more https://firebase.google.com/docs/server/setup#initialize_the_sdk
+// config.json should contain the Firebase service account keys
+// read more https://firebase.google.com/docs/server/setup#initialize_the_sdk
 firebase.initializeApp({
   serviceAccount: config,
-  databaseURL: "https://project-6243802502502885389.firebaseio.com",
+  databaseURL: 'https://project-6243802502502885389.firebaseio.com',
 });
 
-
 // Validate token
-function isValidToken(string) {
-  if (string.match(/([\w\-\.]+)/g)) {
-    console.log('Token is valid')
+function isValidToken(panoptesToken) {
+  if (panoptesToken.match(/([\w\-\.]+)/g)) {
+    console.log('Token is valid');
     return true;
   }
-  else {
-    return false;
-  }
+  return false;
 }
 
 // Get current Panoptes user
-function getApiUser(token) {
+function getPanoptesUser(panoptesToken) {
   console.log('Getting user');
   return fetch(API_URL + '/me', {
     method: 'GET',
     mode: 'cors',
     headers: new Headers({
       'Accept': 'application/vnd.api+json; version=1',
-      'Authorization': 'Bearer ' + token,
+      'Authorization': 'Bearer ' + panoptesToken,
       'Content-Type': 'application/json'
     })
   })
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    var user = json.users[0];
+  .then(response => response.json())
+  .then(json => {
+    const user = json.users[0];
     console.log('Got user: ', user.login, user.id);
     return user;
   })
-  .catch(function(error) {
+  .catch(error => {
     console.error('Failed to get user. Error: ', error);
     throw error;
   });
@@ -57,29 +52,28 @@ function getApiUser(token) {
 
 
 // App
-var app = express();
+const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.get('/validate', function (req, res, next) {
-  var apiToken = req.query.token;
-  if (isValidToken(apiToken)) {
-    getApiUser(apiToken).then(function(user) {
-      const token = firebase.auth().createCustomToken(user.login);
-      res.json({ token });
-    })
-    .catch(function(err) {
-      res.status(403).send({error: 'Failed to get API user: ' + err});
-    });
+app.get('/validate', function(req, res, next) {
+  const panoptesToken = req.query.token;
+  if (isValidToken(panoptesToken)) {
+    getPanoptesUser(panoptesToken)
+      .then(user => {
+        const firebaseToken = firebase.auth().createCustomToken(user.login);
+        res.json({ token: firebaseToken });
+      })
+      .catch(error => res.status(403).send({
+        error: 'Failed to get API user: ' + error
+      }));
   } else {
-    res.status(422).send({error: 'Invalid token'});
+    res.status(422).send({ error: 'Invalid token' });
   }
 });
 
-app.listen(PORT, function () {
-  console.log('==> Listening on port ' + PORT);
-});
+app.listen(PORT, () => console.log('==> Listening on port ' + PORT));
