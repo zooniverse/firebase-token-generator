d19abc3af353a5f08c7b2cdb1b51f35071247f84
Simone Duca <simone@zooniverse.org>
2016-11-24 16:05:16 +0000

    Remove string from token

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
 });
 
 app.post('/validate', function(req, res, next) {
-  const panoptesToken = req.headers.authorization;
+  const panoptesToken = req.headers.authorization.replace('Bearer ', '');;
   if (isValidToken(panoptesToken)) {
     getPanoptesUser(panoptesToken)
       .then(user => {
